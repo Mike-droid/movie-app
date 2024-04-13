21c11733cb4d63ce0ce464b1491ddec6891a800f
mike-droid <miguelreyesmoreno@hotmail.com>
2024-04-13 18:49:05 -0500

    showing days of the week

---
+++ src/app/components/day.js
@@ -0,0 +1,10 @@
+export const Day = ({ day }) => {
+	return (
+		<fieldset>
+			<label htmlFor={day} className='bg-white w-f h-auto text-black cursor-pointer'>
+				{day}
+				<input type='radio' id={day} name="daysWeek" className="" />
+			</label>
+		</fieldset>
+	);
+};
--- src/app/movie/[id]/page.js
+++ src/app/movie/[id]/page.js
@@ -1,4 +1,7 @@
-import { moviesUrl, fetchingOptions } from "@/utils/consts";
+import Image from 'next/image';
+import Link from 'next/link';
+import { moviesUrl, fetchingOptions, moviesImagesUrl } from '@/utils/consts';
+import { Day } from '@/app/components/day';
 
 const fetchMovieDetails = async (id) => {
 	const url = `${moviesUrl}/movie/${id}?language=en-US`;
@@ -12,16 +15,55 @@ const fetchMovieDetails = async (id) => {
 		console.error('error fetching movie details: ', error);
 		throw new Error('Failed to fetch the movie details: ', error);
 	}
-}
+};
 
 export default async function Page({ params }) {
 	const { id } = params;
 	const movieData = await fetchMovieDetails(id);
 
+	const daysOfTheWeek = [
+		'Domingo', // Sunday
+		'Lunes', // Monday
+		'Martes', // Tuesday
+		'Miércoles', // Wednesday
+		'Jueves', // Thursday
+		'Viernes', // Friday
+		'Sábado', // Saturday
+	];
+
+	//* Getting the 7 days of the current week
+	const currentDate = new Date();
+	const nextSevenDays = Array.from({ length: 7 }, (_, index) => {
+		const date = new Date();
+		date.setDate(currentDate.getDate() + index);
+		return date;
+	});
+
 	return (
 		<div>
-			<h1>movie details page</h1>
-			{/*}<p>movie id: {id}</p>*/}
+			<h1>Selecciona el día y horario de la función</h1>
+			<section>
+				<aside>
+					<Image
+						src={`${moviesImagesUrl}${movieData.poster_path}`}
+						width={200}
+						height={200}
+						alt={movieData.title}
+					/>
+					<span>{movieData.overview}</span>
+				</aside>
+				<aside>
+					{nextSevenDays.map((date, index) => (
+						<Day
+							key={index}
+							day={`${daysOfTheWeek[date.getDay()]}, ${date.toLocaleDateString(
+								'es-ES',
+								{ month: 'long', day: 'numeric' }
+							)}`}
+						/>
+					))}
+				</aside>
+			</section>
 		</div>
 	);
 }
